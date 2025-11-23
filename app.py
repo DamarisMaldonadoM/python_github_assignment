bd8565659707fea4b37034aa4443afce61a625fa
DamarisMaldonadoM <damarism@usf.edu>
2025-11-22 21:53:01 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 print("ദ്ദി(ᵔᗜᵔ) Hi, welcome to my python program °‧ 𓆝 𓆟 𓆞 ·｡")
+#This prints out a welcome message to the user.
 
 quantity = input("How many cupcakes did you sell today? ")
-
-quantity = float(quantity)
-price = 3.50
-Total_sales = price * quantity
-
-print(f"Your total sales for cupcake sales is ${Total_sales:.2f} today. 𓆝 𓆟 𓆞 ")
+#This opens up a box so user can iput the number of cupcakes sold.
 
 try:
-    quantity = float(quantity)
+    quantity = float(quantity) #this converts the inputed amount which is a string into a float.
 except ValueError:
     print("Please enter a valid number for the quantity of cupcakes sold.")
     exit()
+#This helps remind users to input a valid number for the input with a message.
+#This also goes after the input.
+
+price = 3.50 #We want to know how much a single cupcake is
+Total_sales = price * quantity #This calculates a total sale so it multiplys the price of $3.50  by the input in float form.
+
+print(f"Your total sales for cupcake sales is ${Total_sales:.2f} today. 𓆝 𓆟 𓆞 ")
+#This prints a messages saying the total sales is [blank] since each input could be different.
+
