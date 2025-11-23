466eb5843e932db81d0e45d44c1a84af48f02df3
DamarisMaldonadoM <damarism@usf.edu>
2025-11-22 21:10:00 -0500

    Add calculation based on user input

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 print("ദ്ദി(ᵔᗜᵔ) Hi, welcome to my python program °‧ 𓆝 𓆟 𓆞 ·｡")
 
-sales = input("How many cupcakes did you sell today?")
+quantity = input("How many cupcakes did you sell today? ")
+
+quantity = float(quantity)
+price = 3.50
+Total_sales = price * quantity
